5795aaac78b613cb71d21d3be7ee677fb74c0001
darshan <darshan.patel.tops@gmail.com>
2023-10-17 08:42:12 +0530

    update

---
+++ C_Language/firstprogram.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+
+// there are 2 types of function
+//	1 User Defined
+	// 1 With Parameter
+	// 2 Without Parameter
+// 	2 System Defined
+	// 1 With Parameter
+	// 2 Without Parameter
+main()
+{
+	printf("Welcome to lecture of C Language");
+}
